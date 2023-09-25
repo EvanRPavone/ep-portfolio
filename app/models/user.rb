41b52d5458efe5088fa425cbf91d47704a7e050d
EvanRPavone <EvanRPavone@gmail.com>
2023-09-25 12:59:24 -0400

    setup User with devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Relations
+  has_many :projects
+  has_one :profile
+end
