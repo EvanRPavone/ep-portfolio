3905bc09586f8666d03ce3f6247a991f0d8c1f5c
EvanRPavone <EvanRPavone@gmail.com>
2023-09-25 16:43:59 -0400

    Home page and about page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # Pages
+  root "pages#home"
+  get 'about', to:'pages#about'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
