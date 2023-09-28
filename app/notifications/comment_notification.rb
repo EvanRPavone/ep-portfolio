148a0e0b0858a86ffa413055583405da1997e72d
EvanRPavone <EvanRPavone@gmail.com>
2023-09-28 12:00:37 -0400

    setup notifications

---
+++ app/notifications/comment_notification.rb
@@ -0,0 +1,30 @@
+# To deliver this notification:
+#
+# CommentNotification.with(post: @post).deliver_later(current_user)
+# CommentNotification.with(post: @post).deliver(current_user)
+
+class CommentNotification < Noticed::Base
+  # Add your delivery methods
+  #
+  deliver_by :database
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  # param :post
+
+  # Define helper methods to make rendering easier.
+  #
+  def message
+    @project = Project.find(params[:comment][:project_id])
+    @comment = Comment.find(params[:comment][:id])
+    @user = User.find(@comment.user_id)
+    "#{@user.username} commented on #{@project.title.truncate(10)}"
+  end
+  #
+  def url
+    project_path(Project.find(params[:comment][:project_id]))
+  end
+end
