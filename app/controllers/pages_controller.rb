2163746774b1f714901a6ce9d0fb70e260b4f6d9
EvanRPavone <EvanRPavone@gmail.com>
2023-10-04 15:18:15 -0400

    home page showing recent projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     # Create a scope that gets the most liked Portfolio or Project
-    @projects = Project.all
+    @projects = Project.includes(:screenshots_attachments, :rich_text_description).order(created_at: :desc).first(6)
   end
 
   def about
