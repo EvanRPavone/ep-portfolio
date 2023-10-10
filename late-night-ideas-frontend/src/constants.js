ea2b6d3e54e9d8c5493b851ae5a05614da4a63b0
EvanRPavone <EvanRPavone@gmail.com>
2023-10-10 15:31:05 -0400

    update gitignore

--- late-night-ideas-frontend/src/constants.js
+++ late-night-ideas-frontend/src/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === "test" 
                         ? "http://mocked-api-url" 
-                        : import.meta.env.VITE_API_URL;
+                        : import.meta.env.FRONTEND_API_URL;
