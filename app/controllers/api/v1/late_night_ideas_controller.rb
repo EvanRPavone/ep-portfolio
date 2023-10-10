36005fee0d256e70e5baaed6d9ad3ad461d13345
EvanRPavone <EvanRPavone@gmail.com>
2023-10-10 15:16:31 -0400

    Added the Late Night Ideas frontend app

--- app/controllers/api/v1/late_night_ideas_controller.rb
+++ app/controllers/api/v1/late_night_ideas_controller.rb
@@ -1,44 +1,44 @@
 class Api::V1::LateNightIdeasController < ApplicationController
   before_action :set_late_night_idea, only: %i[ show update destroy ]
 
-  # GET /late_night_ideas
-  # GET /late_night_ideas.json
+  # GET /api/v1/late_night_ideas
+  # GET /api/v1/late_night_ideas.json
   def index
     @late_night_ideas = LateNightIdea.order(created_at: :desc)
 
     render json: @late_night_ideas
   end
 
-  # GET /late_night_ideas/1
-  # GET /late_night_ideas/1.json
+  # GET /api/v1/late_night_ideas/1
+  # GET /api/v1/late_night_ideas/1.json
   def show
     render json: @late_night_idea
   end
 
-  # POST /late_night_ideas
-  # POST /late_night_ideas.json
+  # POST /api/v1/late_night_ideas
+  # POST /api/v1/late_night_ideas.json
   def create
     @late_night_idea = LateNightIdea.new(late_night_idea_params)
 
     if @late_night_idea.save
-      render :show, status: :created, location: @late_night_idea
+      render json: @late_night_idea, status: :created, location: @late_night_idea
     else
       render json: @late_night_idea.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /late_night_ideas/1
-  # PATCH/PUT /late_night_ideas/1.json
+  # PATCH/PUT /api/v1/late_night_ideas/1
+  # PATCH/PUT /api/v1/late_night_ideas/1.json
   def update
     if @late_night_idea.update(late_night_idea_params)
-      render :show, status: :ok, location: @late_night_idea
+      render json: @late_night_idea
     else
       render json: @late_night_idea.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /late_night_ideas/1
-  # DELETE /late_night_ideas/1.json
+  # DELETE /api/v1/late_night_ideas/1
+  # DELETE /api/v1/late_night_ideas/1.json
   def destroy
     @late_night_idea.destroy
   end
--- late-night-ideas-frontend/src/App.jsx
+++ late-night-ideas-frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import IdeasList from "./features/late-night-ideas/IdeasList";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <h1>Portfoliyours</h1>
+        <p>Find this application layout in late-night-ideas-frontend/src/App.jsx</p>
+        <IdeasList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
